56f438cb01f7863de745293d815696fd4b2b768b
vwvwvv <s22u08l31p@gmail.com.tw>
2023-06-04 10:28:28 +0800

    testDeployWork

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -4,7 +4,7 @@
   <CodeWrapSet />
   <div id="not-found" class="text-center">
 
-    <h1>" 404頁面 "</h1>
+    <h1>" 404 error "</h1>
     <!-- <router-link to="/"></router-link> -->
     <v-btn to="/" size="x-large" width="80%" type="submit" variant="outlined" color="blue" class="mx-auto mt-5">回首頁</v-btn>
   </div>
